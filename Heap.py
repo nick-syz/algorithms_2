398fff6ec9340dfdc318030ea35105bd06cc8986
Nick <nicksyz13@gmail.com>
2021-03-08 12:14:57 +0300

    All changes

--- Heap.py
+++ Heap.py
@@ -4,28 +4,8 @@ class Heap:
 
     def __init__(self):
         self.HeapArray = []
-    
-    def MakeHeap(self, a, depth):
-        self.HeapArray = [None] * (2**(depth+1)-1)
-        for i in a:
-            self.Add(i)
-    
-    def GetMax(self):
-        if len(self.HeapArray) and self.HeapArray[0] is not None:
-            max_key = self.HeapArray[0]
-            i = len(self.HeapArray)-1
-            while i > 0:
-                if self.HeapArray[i] is not None:
-                    break
-                i -= 1
-            if i != 0:
-                self.HeapArray[0], self.HeapArray[i] = \
-                    self.HeapArray[i], None
-                self.SiftDown(0, self.HeapArray)
-            else:
-                self.HeapArray[0] = None
-            return max_key
-        return -1
+        self.count = 0
+        self.free_index = None
     
     def Swap(self, i, j):
         self.HeapArray[i], self.HeapArray[j] = \
@@ -36,35 +16,67 @@ class Heap:
         right = 2*i+2
         largest = i
         size = len(array)
-        if left < size:
-            if array[left] is not None and array[left] > array[largest]:
-                largest = left
-        if right < size:
-            if array[right] is not None and array[right] > array[largest]:
-                largest = right
+        if left < size and array[left] != None and array[left] > array[largest]:
+            largest = left
+        if right < size and array[right] != None and array[right] > array[largest]:
+            largest = right
         if largest != i:
             self.Swap(largest, i)
             return self.SiftDown(largest, array)
-    
+
     def SiftUp(self, i):
-        while self.HeapArray[(i-1)//2] is None or \
-                self.HeapArray[i] > self.HeapArray[(i-1)//2]:
+        while i > 0 and (self.HeapArray[(i-1)//2] is None or \
+                self.HeapArray[i] > self.HeapArray[(i-1)//2]):
             self.Swap((i-1)//2, i)
             i = (i-1)//2
-            if (i-1)//2 < 0:
-                break
 
+    def MakeHeap(self, array, depth):
+        if len(array) <= (2**(depth+1)-1):
+            self.HeapArray = array[:]
+            self.count = len(array)
+            length = 2**(depth+1)-1
+            for i in range(length-len(array)):
+                self.HeapArray.append(None)
+            for key in range(self.count-1,-1,-1):
+                self.SiftUp(key)
+            if len(array) < len(self.HeapArray):
+                self.free_index = len(array)
+
+    def GetMax(self):
+        if self.count:
+            max_key = self.HeapArray[0]
+            i = self.count-1
+            self.HeapArray[0], self.HeapArray[i] = \
+                self.HeapArray[i], None
+            self.SiftDown(0, self.HeapArray)
+            self.count -= 1
+            return max_key
+        return -1
+    
     def Add(self, key):
-        i = len(self.HeapArray)-1
-        if self.HeapArray[i] is None:
-            self.HeapArray[i] = key
+        if self.count == len(self.HeapArray):
+            return False
+        i = self.free_index
+        if i < len(self.HeapArray)-1:
+            self.free_index += 1
         else:
-            while i > 0:
-                i -= 1
-                if self.HeapArray[i] is None:
-                    break
-            if i == 0:
-                return False
-            self.HeapArray[i] = key
+            self.free_index = None
+        self.count += 1
+        self.HeapArray[i] = key
         self.SiftUp(i)
+        return True
+    
+    def CheckHeap(self, i=0):
+        left = 2*i+1
+        right = 2*i+2
+        array = self.HeapArray
+        size = len(array)
+        if left < size and array[left] != None and array[left] > array[i]:
+            return False
+        if right < size and array[right] != None and array[right] > array[i]:
+            return False
+        if 2*i+1 < size:
+            return self.CheckHeap(2*i+1)
+        if 2*i+2 < size:
+            return self.CheckHeap(2*i+2)
         return True
